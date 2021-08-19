6e5c5cfbccaffb5ce14ad8fc3aebcfdd322ead11
Trevor <trevorwerner@hotmail.com>
2021-08-19 06:25:23 -0400

    Added handleInteraction, removeLife methods to Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,53 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+   constructor() {
+      this.missed = 0;
+      // array of five phrases to choose from
+      this.phrases = ["I'll be back", "Luke, I am your father", "Release the Kraken!", "Bond, James Bond", "Say hello to my little friend!"];
+      this.activePhrase = null;
+   }
+
+   startGame() {
+      // hide the start screen overlay
+      document.getElementById('overlay').style.display = 'none';
+      // sets activePhrase to one of the 5 phrases
+      const phrase = this.getRandomPhrase();
+      this.activePhrase = new Phrase(phrase);
+   }
+
+   getRandomPhrase() {
+      const idx = Math.floor(Math.random() * this.phrases.length);
+      return this.phrases[idx];
+   }
+
+   /**
+    * Checks to see if the button clicked by the player matches a 
+    * letter in the activePhrase
+    */
+   handleInteraction(keys, letter) {   
+      const phrase = this.activePhrase.phrase; 
+      if (!phrase.includes(letter)) {
+         for (let key of keys) {
+            if (key.textContent === letter) {
+               key.classList.add('wrong');
+            }
+         }
+         this.removeLife();
+      } else {
+         for (let key of keys) {
+            if (key.textContent === letter) {
+               key.classList.add('chosen');
+            }
+         }
+         this.showMatchedLetter();
+         const hasWon = this.checkForWin();
+         if (hasWon) {
+            this.gameOver();
+         }
+      }
+   }
+
+   removeLife() {
+      const hearts = document.querySelectorAll("img[src='images/liveHeart.png']")
+      hearts[0].setAttribute('src', "images/lostHeart.png");
+   }
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+const startBtn = document.getElementById('btn__reset');
+const keyBtns = document.getElementsByClassName('key');
+const game = new Game();
+
+for (let key of keyBtns) {
+   key.addEventListener('click', (e) => {
+      game.handleInteraction(keyBtns, e.target.textContent);
+      key.setAttribute('disabled', true);
+   });
+   // TODO: would like to add ability to type letter, in addition to using mouse
+}
+
+startBtn.addEventListener('click', () => {
+   game.startGame();
+});
+

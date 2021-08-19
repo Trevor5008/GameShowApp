22c6dfe4f6e31a833807781f874fbb57d77a2bfd
Trevor <trevorwerner@hotmail.com>
2021-08-18 23:24:35 -0400

    Added stubs and pseudocode for phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,30 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+   constructor(phrase) {
+      this.phrase = phrase.toLowerCase();
+   }
+
+   /**
+    * Adds letter placeholders to the display
+    * when the game starts (each letter represented
+    * by an empty (li) box).
+    */
+   addPhraseToDisplay() {
+
+   }
+
+   /**
+    * Checks to see if the letter selectedby the player matches
+    * a letter in the phrase
+    */
+   checkLetter() {
+
+   }
+
+   /**
+    * Reveals the letter(s) on the board that match the 
+    * player's selection
+    */
+   showMatchedLetter() {
+
+   }
+};
